93bbfa086cec0c70905e84b729b8a2c5b1e12837
elainemwang <32891543+elainemwang@users.noreply.github.com>
2019-06-04 08:48:22 -0700

    main screen and method created

---
+++ src/combined/run.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combined;
+
+import java.io.FileNotFoundException;
+import minesweeper.*;
+import tictactoe.*;
+import kelly8282.flappybird.*;
+
+
+/**
+ *
+ * @author elainewang
+ */
+public class run {
+    public run(){
+        mainScreen screen = new mainScreen();
+        
+    }
+    
+    public static void main(String args[]) throws FileNotFoundException {
+        Play run = new Play();
+        System.out.println("Starting Flappy Bird - 1.1 by Kelly");
+        FlappyBird flappyBird = new FlappyBird();
+        
+        new InitialScreen();
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Game.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+              //  new Game().setVisible(true);
+            }
+        });
+    }
+}
